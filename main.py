531b105a726afe4676c6b70936e0d72b4c3c0b2f
amel-am <96318827+amel-am@users.noreply.github.com>
2024-08-01 05:39:48 +0200

    Changed a lot of logic in the script. Also removed checking for instance because of type annotation.

--- main.py
+++ main.py
@@ -4,14 +4,25 @@ from typing import Union, Optional
 
 class Circle:
     def __init__(self, radius: Optional[Union[int, float]] = None, diameter: Optional[Union[int, float]] = None):
-        if isinstance(radius, (int, float)):
-            self.__radius_diameter = radius
-        elif isinstance(diameter, (int, float)):
-            self.__radius_diameter = diameter
+        if radius:
+            if radius < 0:
+                raise ValueError("Radius cannot be zero or negative.")
+            self.__radius = radius
+        elif diameter:
+            if diameter < 0:
+                raise ValueError("Diameter cannot be zero or negative.")
+            self.__radius = diameter
+        else:
+            raise ValueError(
+                "A non zero-negative value has to be provided to either radius or diameter. Make also sure to provide the right type, either int or float.")
+        self.__diameter = self.__radius / 2
+        # Has to be floating value, because of type checker.
+        self.__area = 0.0
 
-    def calculate_area(self) -> float:
+    def calculate_area(self) -> Union[float]:
         # Calculates the area of the circle and round by 2.
-        return round(self.__radius ** 2 * pi, 2)
+        self.__area = round(self.__radius ** 2 * pi, 2)
+        return self.__area
 
     @property
     def radius(self) -> Optional[Union[int, float]]:
@@ -21,14 +32,10 @@ class Circle:
     @radius.setter
     def radius(self, radius: Union[int, float]) -> None:
         # Raises ValueError if radius is either equal to zero or less (negative number).
-        if not isinstance(radius, (int, float)):
-            raise ValueError("The value provided is not of type float or int.")
-        elif radius <= 0:
+        if radius <= 0:
             raise ValueError(
                 "The provided value has to be positive and not zero.")
-        else:
-            self.__radius = radius
-            self.__diameter = self.__radius * 2
+        self.__radius = radius
 
     @property
     def diameter(self) -> Optional[Union[int, float]]:
@@ -37,78 +44,61 @@ class Circle:
 
     @diameter.setter
     def diameter(self, diameter: Union[int, float]) -> None:
-        # Raises ValueError if the newly assigned value of diameter is not of type float or int.
-        if not isinstance(diameter, (int, float)):
-            raise ValueError("The value provided is not of type float or int.")
         # Raises ValueError if diameter is either equal to zero or less (negative number).
-        elif diameter <= 0:
+        if diameter <= 0:
             raise ValueError(
                 "The provided value has to be positive and not zero.")
         else:
             self.__diameter = diameter
             self.__radius = self.__diameter / 2
 
+    @property
+    def area(self) -> Optional[Union[int, float]]:
+        return self.__area
 
-class PizzaCalculator(Circle):
-    def __init__(self, small_pizza_radius: Union[int, float] = 1, small_pizza_diameter: Union[int, float] = 1,
-                 large_pizza_radius: Union[int, float] = 1, large_pizza_diameter: Union[int, float] = 1):
-        self.__small_pizza_radius = small_pizza_radius
-        self.__small_pizza_diameter = small_pizza_diameter
-        self.__large_pizza_radius = large_pizza_radius
-        self.__large_pizza_diameter = large_pizza_diameter
-        super().__init__(self.__small_pizza_radius, self.__small_pizza_diameter)
+
+class PizzaComparison(Circle):
+    def __init__(self, small_pizza_radius: Optional[Union[int, float]] = None, small_pizza_diameter: Optional[Union[int, float]] = None,
+                 large_pizza_radius: Optional[Union[int, float]] = None, large_pizza_diameter: Optional[Union[int, float]] = None):
+        super().__init__(small_pizza_radius, small_pizza_diameter)
         self.__small_pizza_area = self.calculate_area()
-        super().__init__(self.__large_pizza_radius, self.__large_pizza_diameter)
+        super().__init__(large_pizza_radius, large_pizza_diameter)
         self.__large_pizza_area = self.calculate_area()
 
-    @property
-    def small_pizza_area(self):
-        return self.__small_pizza_area
-
-    @property
-    def small_pizza_radius(self):
-        return self.__small_pizza_radius
-
-    @property
-    def small_pizza_diameter(self):
-        return self.__small_pizza_diameter
-
-    @property
-    def large_pizza_radius(self):
-        return self.__large_pizza_radius
-
-    @property
-    def large_pizza_diameter(self):
-        return self.__large
-
-    @property
-    def large_pizza_area(self):
-        return self.__large_pizza_area
-
-    def compare_price(self, small_pizza_amount: Union[int, float] = 1, small_pizza_price: Union[int, float] = 4,
-                      large_pizza_amount: Union[int, float] = 1, large_pizza_price: Union[int, float] = 6):
-        if isinstance(small_pizza_amount, (int, float)) and isinstance(large_pizza_amount, (int, float)):
-            if small_pizza_amount <= 0 or large_pizza_amount <= 0:
-                raise ValueError(
-                    "Amount of large pizza or small can't be equal to zero or negative.")
-        if isinstance(small_pizza_price, (int, float)) and isinstance(large_pizza_price, (int, float)):
-            if small_pizza_amount <= 0 or large_pizza_amount <= 0:
-                raise ValueError(
-                    "Cost of large pizza or small can't be equal to zero or negative.")
-        small_pizza_price_amount = small_pizza_amount * small_pizza_price
-        large_pizza_price_amount = large_pizza_amount * large_pizza_price
-        small_pizza_area_amount = self.small_pizza_area * small_pizza_amount
-        large_pizza_area_amount = self.large_pizza_area * large_pizza_amount
-        if small_pizza_price_amount > large_pizza_price_amount:
-            if small_pizza_area_amount > large_pizza_area_amount:
-                return "You are getting more pizza but it costs more."
+    def compare_pizzas_price(self, small_pizza_price: Union[int, float],  large_pizza_price: Union[int, float],
+                             small_pizza_amount: Union[int, float] = 1, large_pizza_amount: Union[int, float] = 1) -> Optional[str]:
+        if small_pizza_price <= 0 or large_pizza_price <= 0:
+            raise ValueError(
+                "The price for the small pizza or the large pizza cannot be equal to zero or a negative number.")
+        elif small_pizza_amount <= 0 or large_pizza_amount <= 0:
+            raise ValueError(
+                "The amount of the small pizza or large pizza cannot be equal to zero or a negative number.")
+        small_pizza_amount_price = small_pizza_price * small_pizza_amount
+        small_pizza_amount_area = self.__small_pizza_area * small_pizza_amount
+        large_pizza_amount_price = large_pizza_price * large_pizza_amount
+        large_pizza_amount_area = self.__large_pizza_area * large_pizza_amount
+        if large_pizza_amount_price > small_pizza_amount_price:
+            if large_pizza_amount_area > small_pizza_amount_area:
+                price_difference = large_pizza_amount_price - small_pizza_amount_price
+                area_difference = large_pizza_amount_area - small_pizza_amount_area
+                result = "The large pizza is more worth it. "
             else:
-                return "You are getting less pizza and it even costs more, what a waste."
+                price_difference = large_pizza_amount_price - small_pizza_amount_price
+                area_difference = small_pizza_amount_area - large_pizza_amount_area
+                result = "The large pizza is not worth it. "
         else:
-            print("hello")
+            if small_pizza_amount_area > large_pizza_amount_area:
+                price_difference = small_pizza_amount_price - large_pizza_amount_price
+                area_difference = small_pizza_amount_area - large_pizza_amount_area
+                result = "The small pizza is definitely worth it. "
+            else:
+                price_difference = small_pizza_amount_price - large_pizza_amount_price
+                area_difference = large_pizza_amount_area - small_pizza_amount_area
+                result = "The small pizza is not worth it. "
+        return result + f"The price difference is ${price_difference:.2f} and the area difference is {area_difference:.2f}cm"
 
-            # Example usage
-kreis = Circle(5)
-pizza = PizzaCalculator(small_pizza_radius=3, large_pizza_radius=6)
-print(pizza.small_pizza_area)
-print(pizza.large_pizza_area)
+
+        # Example usage
+pizza = PizzaComparison(small_pizza_radius=6, large_pizza_radius=4)
+print(pizza.compare_pizzas_price(7, 4, 1, 1))
+# prints -> "The small pizza is definitely worth it."
